9db0b62ff589109c983aa235349df1f910def286
Jirka Hladky <jhladky@redhat.com>
2021-04-09 00:14:52 +0200

    Multiply random numbers

--- avx_fma.c
+++ avx_fma.c
@@ -43,30 +43,33 @@ static __inline__ u_int64_t stop_clock() {
 int main () {
   signal(SIGINT, inthand);
   signal(SIGTERM, inthand);
+  u_int64_t diff = 0;
+  u_int64_t rdtsc;
 
   double a[4];
   double b[4];
   double c[4];
 
   for (int i = 0; i < 4; i++) {
-    a[i] = drand48();
-    b[i] = drand48();
     c[i] = 0.0;
   }
   a[0] = 1.0;
   b[0] = 1.0;
 
-  __m256d av = _mm256_loadu_pd(a);      // load 4 double from a
-  __m256d bv = _mm256_loadu_pd(b);      // load 4 double from b
   __m256d cv = _mm256_loadu_pd(c);      // load 4 double from c
 
   printf("Starting computation. Use Ctrl-C to stop it.\n");
-  u_int64_t start_rdtsc = start_clock();
   while (!stop) {
+    for (int i = 1; i < 4; i++) {
+      a[i] = drand48();
+      b[i] = drand48();
+    }
+    __m256d av = _mm256_loadu_pd(a);      // load 4 double from a
+    __m256d bv = _mm256_loadu_pd(b);      // load 4 double from b
+    rdtsc = start_clock();
     cv =  _mm256_fmadd_pd(av, bv, cv); // cv = av*bv + cv
+    diff += (stop_clock() - rdtsc);
   }
-  u_int64_t stop_rdtsc = stop_clock();
-  u_int64_t diff = (stop_rdtsc-start_rdtsc);
   _mm256_storeu_pd(c, cv);             // write cv to c array
 
 
